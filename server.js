3586500446a5a393cf391d3e951748257d38bb72
robotnikz <tobiashalbei@mailbox.org>
2026-01-13 08:24:34 +0100

    fix(server): express 5 catch-all route

--- server.js
+++ server.js
@@ -1957,7 +1957,7 @@ app.post('/api/preview-csv', upload.single('file'), (req, res) => {
     });
 });
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
